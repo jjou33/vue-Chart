134e05b6cb2e157a9f266fea2fd73627fa6111de
hyunwoo <zanda33@naver.com>
2022-06-27 00:15:26 +0900

    Refactor: 외부 라이브러리 컴포넌트화 및 Ref 적용

--- README.md
+++ README.md
@@ -1,24 +1,11 @@
 # vue-chart
 
-## Project setup
-```
-npm install
-```
+## Chart.js 학습을 위한 간단한 실습용 레포
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+---
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
+`외부 라이브러리 모듈화`를 위해서 실습 간 `Chart.js`를 실습하는 내용을 업로드 한다.
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/LineChart.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <canvas ref="myLineChart"></canvas>
+  </div>
+</template>
+
+<script>
+import { Chart, registerables } from "chart.js";
+
+Chart.register(...registerables);
+
+export default {
+  mounted() {
+    const labels = ["January", "February", "March", "April", "May", "June"];
+
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: "My First dataset",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: [0, 10, 5, 2, 20, 30, 45],
+        },
+      ],
+    };
+
+    const config = {
+      type: "line",
+      data: data,
+      options: {},
+    };
+    const myLineChart = new Chart(this.$refs.myLineChart, config);
+  },
+};
+</script>
+
+<style></style>
